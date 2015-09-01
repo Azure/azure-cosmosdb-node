08b80c6748219cb98b35a702ae00d2e7613b0e35
Andrew Liu <aliuy8@gmail.com>
2015-09-01 12:54:16 -0700

    Clean github pages directory

--- core_sdk/test/_testConfig.js
+++
@@ -1,5 +0,0 @@
-var host = "MyHost";                                                
-var key = "MyKey";
-
-exports.host = host;
-exports.masterKey = key;
--- samples/readme.md
+++
@@ -1,1 +0,0 @@
-These are the samples for the Node.js driver for Azure DocumentDB
